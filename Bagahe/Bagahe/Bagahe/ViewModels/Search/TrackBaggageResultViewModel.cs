084e099ec1b5a048f06a95239628154c12780d81
jilbert.gacayan <jilbert.gacayan@pointwest.com.ph>
2017-09-15 00:31:46 +0800

    Initial set of changes for the track baggage function.

---
+++ Bagahe/Bagahe/Bagahe/ViewModels/Search/TrackBaggageResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bagahe.ViewModels.Search
+{
+    class TrackBaggageResultViewModel : BaseViewModel
+    {
+    }
+}
